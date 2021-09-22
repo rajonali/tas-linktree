3b0d2b21bea1eb6316152e47823f60be860dbf43
unknown <rajonali44492@icloud.com>
2021-09-22 04:39:43 -0500

    More styling

--- components/Education.js
+++ components/Education.js
@@ -4,7 +4,7 @@ import EducationCard from "./EducationCard"
 
 function Education({educationInfo}) {
     return (
-        <div className="grid p-5">
+        <div className="grid p-5 pt-20">
           <h1 className="self-center text-start text-5xl py-10 font-bold tracking-tight text-black-500 mb-5 border-l-8 h-5.5 border-yellow-200 px-5">Education</h1>
              <div className="py-10">
               {educationInfo.schools.map((school, index) => (

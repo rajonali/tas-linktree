6019b08ff8352bebfd77a41f00cce1ddc0e6c296
unknown <rajonali44492@icloud.com>
2021-09-23 22:52:14 -0500

    More styling

--- components/Skills.js
+++ components/Skills.js
@@ -4,14 +4,16 @@ import {DatabaseIcon,PencilIcon, AcademicCapIcon, LightningBoltIcon} from '@hero
 function Skills() {
     return (
 
-<div className="pl-5">
-        <h1 className="text-start text-5xl pb-10 font-bold tracking-tight text-black-500 mb-5 border-l-8 h-5.5 border-yellow-200 px-5">Skills Overview</h1>
+<div className="m-5">
+        <h1 className="text-start text-5xl pb-1 leading-tight font-bold tracking-tight text-black-500 mb-5 border-l-8 mb-10 h-5.5 border-yellow-200 px-5">Skills Overview</h1>
 
-        <div className="grid md:grid-cols-2 gap-8 mx-2 justify-center">
+        <div className="grid md:grid-cols-2 gap-8 justify-center">
             
             
             
             
+
+
             <div className="rounded-md shadow-lg border-dashed border-4 border-light-blue-500 mx-5 ">
 
 <div className="flex justify-center items-center">
@@ -20,14 +22,12 @@ function Skills() {
 <div className="flex-col px-5">
 <h2 className="text-2xl font-bold">Frontend Development</h2>
 
-<p className="m-2">Efficient deployable responsive development using the latest and the greatest</p>
+<p>Efficient deployable responsive development using the latest and the greatest</p>
 </div>
 </div>
 
 
-            </div>
-
-
+            </div> 
 
 
             <div className="rounded-md shadow-lg border-dashed border-4 border-light-blue-500 mx-5 ">
@@ -43,7 +43,8 @@ function Skills() {
 </div>
 
 
-            </div> <div className="rounded-md shadow-lg border-dashed border-4 border-light-blue-500 ">
+            </div> 
+            <div className="rounded-md shadow-lg border-dashed border-4 border-light-blue-500 mx-5 ">
 
 <div className="flex justify-center items-center">
 <AcademicCapIcon className="h-36 mb-1 pl-5"/>
@@ -60,7 +61,7 @@ function Skills() {
             
             
 
-            <div className="rounded-md shadow-lg border-dashed border-4 border-light-blue-500 ">
+            <div className="rounded-md shadow-lg border-dashed border-4 border-light-blue-500 mx-5 ">
 
 <div className="flex justify-center items-center">
 <PencilIcon className="h-36 mb-1 pl-5"/>
--- components/WorkExperience.js
+++ components/WorkExperience.js
@@ -9,7 +9,7 @@ function WorkExperience({workExperiences}) {
         <Fade bottom duration={1000} distance="20px">
           <div className="experience-container p-5" id="workExperience">
 
-          <h1 className="flex text-5xl py-5 font-bold tracking-tight text-black-500 mb-5 border-l-8 h-5.5 border-yellow-200 px-5">Experience</h1>
+          <h1 className="flex text-5xl p-5 font-bold tracking-tight text-black-500 mb-5 border-l-8 h-5.5 border-yellow-200 px-5">Experience</h1>
 
               <div className="experience-cards-div py-10 px-5 my-10 sm:grid w-auto gap-10 md:grid-cols-2 xl:grid-cols-3 w-0.6 3xl: flex flex-wrap justify-center" >
                 {workExperiences.experience.map((card, i) => {
